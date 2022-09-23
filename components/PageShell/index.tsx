fe6b5318ad9655aeee111da337e9c17ddbb78b85
Mateus Deitos <mate.deitos@hotmail.com>
2022-09-23 20:46:47 -0300

    Hotjar Script

--- components/PageShell/index.tsx
+++ components/PageShell/index.tsx
@@ -52,21 +52,30 @@ export const PageShell = ({ children, FooterChildren, pageTitle, breadcrumbs }: 
 				</Breadcrumbs>}
 				<Text size="xl" mb="xs" weight={500}>{pageTitle}</Text>
 
+				<Script dangerouslySetInnerHTML={{
+					__html: `
+					(function(h,o,t,j,a,r){
+						h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+						h._hjSettings={hjid:${process.env.NEXT_PUBLIC_HOTJAR_ID},hjsv:${process.env.NEXT_PUBLIC_HOTJAR_SV}};
+						a=o.getElementsByTagName('head')[0];
+						r=o.createElement('script');r.async=1;
+						r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+						a.appendChild(r);
+					})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+					`
+				}} />
 				<Script
 					src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG_ID}`}
 					onLoad={() => {
 						gaHandler('js', new Date());
 
-						gaHandler('config', 'G-SWXW9P4SYN');
-
+						gaHandler('config', process.env.NEXT_PUBLIC_GTAG_ID);
 					}}
 				/>
 				<Script
 					src={`https://www.googleoptimize.com/optimize.js?id=${process.env.NEXT_PUBLIC_OPTIMIZE_CONTAINER_ID}`}
 				/>
-				<GaProvider value={ga}>
-					{children}
-				</GaProvider>
+				{children}
 			</Box>
 		</AppShell >
 
